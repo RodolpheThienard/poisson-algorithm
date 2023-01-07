8a88f4e4e2086c76b704bee0789a7fe91f52b8e1
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-01-07 17:37:03 +0100

    Adding tp_poisson1D_iter
    Adding tp_poisson1D_iter file which solve poisson1D by using iteration method
    Adding rule to compile it in Makefile
    
    Formating all files with .clang-format

--- src/tp_poisson1D_direct.c
+++ src/tp_poisson1D_direct.c
@@ -5,161 +5,150 @@
 /******************************************/
 #include "lib_poisson1D.h"
 #include "atlas_headers.h"
-#include <bits/time.h>
-#include <lapack.h>
-#include <string.h>
-#include <time.h>
 
 int main(int argc, char *argv[])
 /* ** argc: Nombre d'arguments */
 /* ** argv: Valeur des arguments */
 {
-    i32 ierr;
-    i32 jj;
-    i32 nbpoints, la;
-    i32 ku, kl, kv, lab;
-    i32 *ipiv;
-    i32 info;
-    i32 NRHS;
-    f64 T0, T1;
-    f64 *RHS, *EX_SOL, *X;
-    f64 **AAB;
-    f64 *AB;
+	i32 ierr;
+	i32 jj;
+	i32 nbpoints, la;
+	i32 ku, kl, kv, lab;
+	i32 *ipiv;
+	i32 info;
+	i32 NRHS;
+	f64 T0, T1;
+	f64 *RHS, *EX_SOL, *X;
+	f64 **AAB;
+	f64 *AB;
 
-    f64 * tmp_vec, *sol;
+	f64 *tmp_vec, *sol;
 
-    f64 temp, forward_error, backward_error;
+	f64 temp, forward_error, backward_error;
 
-    struct timespec t1, t2;
-    f64 elapsed_dgbtrs, elapsed_dgbtrf, elapsed_dgbsv, elapsed_dgbtrftridiag;
-    
-    elapsed_dgbsv = 0.0;
-    elapsed_dgbtrs = 0.0;
-    elapsed_dgbtrf = 0.0;
-    elapsed_dgbtrftridiag = 0.0;
+	struct timespec t1, t2;
+	f64 elapsed_dgbtrs, elapsed_dgbtrf, elapsed_dgbsv,
+		elapsed_dgbtrftridiag;
 
-    u32 array_size = 10;
-    if(argc > 1){
-        array_size = atoi(argv[1]);        
-    }
-    
-    NRHS = 1;
-    nbpoints = array_size;
-//    printf("nb ptn : %d", nbpoints);
-    la = nbpoints - 2;
-    T0 = -5.0;
-    T1 = 5.0;
+	elapsed_dgbsv = 0.0;
+	elapsed_dgbtrs = 0.0;
+	elapsed_dgbtrf = 0.0;
+	elapsed_dgbtrftridiag = 0.0;
 
-//    printf("--------- Poisson 1D ---------\n\n");
-    RHS = (f64 *)malloc(sizeof(f64) * la);
-    EX_SOL = (f64 *)malloc(sizeof(f64) * la);
-    X = (f64 *)malloc(sizeof(f64) * la);
+	u32 array_size = 10;
+	if (argc > 1) {
+		array_size = atoi(argv[1]);
+	}
 
-    sol = malloc (sizeof (double) * la);
-    tmp_vec = malloc (sizeof (double) * la);
+	NRHS = 1;
+	nbpoints = array_size;
+	//    printf("nb ptn : %d", nbpoints);
+	la = nbpoints - 2;
+	T0 = -5.0;
+	T1 = 5.0;
 
-    // TODO : you have to implement those functions
-    set_grid_points_1D(X, &la);
-    set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
-    set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
+	//    printf("--------- Poisson 1D ---------\n\n");
+	RHS = (f64 *)malloc(sizeof(f64) * la);
+	EX_SOL = (f64 *)malloc(sizeof(f64) * la);
+	X = (f64 *)malloc(sizeof(f64) * la);
 
-    write_vec(RHS, &la, "RHS.dat");
-    write_vec(EX_SOL, &la, "EX_SOL.dat");
-    write_vec(X, &la, "X_grid.dat");
+	sol = malloc(sizeof(double) * la);
+	tmp_vec = malloc(sizeof(double) * la);
 
-    kv = 1;
-    ku = 1;
-    kl = 1;
-    lab = kv + kl + ku + 1;
+	// TODO : you have to implement those functions
+	set_grid_points_1D(X, &la);
+	set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
+	set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
 
-    AB = (f64 *)malloc(sizeof(f64) * lab * la);
+	write_vec(RHS, &la, "RHS.dat");
+	write_vec(EX_SOL, &la, "EX_SOL.dat");
+	write_vec(X, &la, "X_grid.dat");
 
-    set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
+	kv = 1;
+	ku = 1;
+	kl = 1;
+	lab = kv + kl + ku + 1;
 
-    write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
+	AB = (f64 *)malloc(sizeof(f64) * lab * la);
 
-//    printf("Solution with LAPACK\n");
-    /* LU Factorization */
+	set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
+	// cblas_dgbmv(CblasColMajor, CblasTrans, la, la, kl, ku, 1, AB + 1, lab, EX_SOL, 1, 0, RHS, 1);
+	write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
 
-    f64 *ABB = (f64*)malloc(sizeof(f64)* lab * la);
-    set_GB_operator_colMajor_poisson1D(ABB, &lab, &la, &kv);
-               
-    info = 0;
-    ipiv = (i32 *)calloc(la, sizeof(i32));
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-    dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-    elapsed_dgbtrf = (f64)(t2.tv_nsec - t1.tv_nsec);
-    /* LU for tridiagonal matrix  (can replace dgbtrf_) */
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-    ierr= dgbtrftridiag(&la, &la, &kl, &ku, ABB, &lab, ipiv, &info);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-    elapsed_dgbtrftridiag = (f64)(t2.tv_nsec - t1.tv_nsec);
+	//    printf("Solution with LAPACK\n");
+	/* LU Factorization */
 
-    write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
+	f64 *ABB = (f64 *)malloc(sizeof(f64) * lab * la);
+	set_GB_operator_colMajor_poisson1D(ABB, &lab, &la, &kv);
 
-    /* Solution (Triangular) */
-    if (info == 0)
-    {
-        clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-        dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-        clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-        elapsed_dgbtrs = (f64)(t2.tv_nsec - t1.tv_nsec);
-        if (info != 0)
-        {
-            printf("\n INFO DGBTRS = %d\n", info);
-        }
-    }
-    else
-    {
-        printf("\n INFO = %d\n", info);
-    }
+	info = 0;
+	ipiv = (i32 *)calloc(la, sizeof(i32));
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+	dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
+	elapsed_dgbtrf = (((f64)(t2.tv_sec - t1.tv_sec)) +
+			  (t2.tv_nsec - t1.tv_nsec) * 10e-9L);
 
-    /* It can also be solved with dgbsv */
-    // TODO : use dgbsv
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-    dgbsv_(&la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-    elapsed_dgbsv = (f64)(t2.tv_nsec - t1.tv_nsec);
+	/* LU for tridiagonal matrix  (can replace dgbtrf_) */
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+	ierr = dgbtrftridiag(&la, &la, &kl, &ku, ABB, &lab, ipiv, &info);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
+	elapsed_dgbtrftridiag = (((f64)(t2.tv_sec - t1.tv_sec)) +
+				 (t2.tv_nsec - t1.tv_nsec) * 10e-9L);
 
-    write_xy(RHS, X, &la, "SOL.dat");
+	write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
 
-    /* Relative forward and backward error */
-    // TODO : Compute relative norm of the residual
+	/* Solution (Triangular) */
+	if (info == 0) {
+		clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+		dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la,
+			&info);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
+		elapsed_dgbtrs = (((f64)(t2.tv_sec - t1.tv_sec)) +
+				  (t2.tv_nsec - t1.tv_nsec) * 10e-9L);
+		if (info != 0) {
+			printf("\n INFO DGBTRS = %d\n", info);
+		}
+	} else {
+		printf("\n INFO = %d\n", info);
+	}
 
-    /* Forward error :    ||x-xø|| / ||x|| */
+	/* It can also be solved with dgbsv */
+	// TODO : use dgbsv
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+	dgbsv_(&la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
+	elapsed_dgbsv = (((f64)(t2.tv_sec - t1.tv_sec)) +
+			 (t2.tv_nsec - t1.tv_nsec) * 10e-9L);
+	write_xy(RHS, X, &la, "SOL.dat");
 
-    memcpy(tmp_vec, RHS, la * sizeof (f64));
+	/* Relative forward and backward error */
+	// TODO : Compute relative norm of the residual
 
-    cblas_daxpy(la, -1, EX_SOL, 1, tmp_vec, 1);
+	/* Forward error :    ||x-xø|| / ||x|| */
 
-    f64 norm_x_xø = cblas_dnrm2(la, tmp_vec, 1);
-    
-    f64 norm_x = cblas_dnrm2(la, EX_SOL, 1);
+	memcpy(tmp_vec, RHS, la * sizeof(f64));
 
-    forward_error = norm_x_xø / norm_x;
+	cblas_daxpy(la, -1, EX_SOL, 1, tmp_vec, 1);
 
+	f64 norm_x_xø = cblas_dnrm2(la, tmp_vec, 1);
 
-    /* Backward error :    ||b-Axø|| / ||b|| */
+	f64 norm_x = cblas_dnrm2(la, EX_SOL, 1);
 
-    memcpy(tmp_vec, RHS, la * sizeof (f64));
+	forward_error = norm_x_xø / norm_x;
 
-    cblas_dgbmv(CblasRowMajor, CblasNoTrans, la, la, kl, ku, -1.0, AB, lab, sol, 1, 1.0, tmp_vec, 1);
-
-    f64 tmp = cblas_dnrm2(la, tmp_vec, 1);
-
-    backward_error = tmp / cblas_dnrm2(la, RHS, 1);
-
-
-//    printf("\nThe relative forward error is relres = %e\n", forward_error );
-//    printf("\nThe relative backward error is relres = %e\n", backward_error );
-    printf("%d : dgbtrf : %f \n \
-            %d : dgbtrs : %f \n \
-            %d : dgbsv : %f\n \
-            %d : dgbtrftridiag : %f\n", nbpoints, elapsed_dgbtrf, nbpoints, elapsed_dgbtrs, nbpoints, elapsed_dgbsv, nbpoints, elapsed_dgbtrftridiag);
-    free(RHS);
-    free(EX_SOL);
-    free(X);
-    free(AB);
-//    printf("\n\n--------- End -----------\n");
+	//    printf("\nThe relative forward error is relres = %e\n", forward_error );
+	printf("%d : dgbtrf : %lf \n \
+            %d : dgbtrs : %lf \n \
+            %d : dgbsv : %lf\n \
+            %d : dgbtrftridiag : %lf \n \
+        %d : forward_error : %lf\n",
+	       nbpoints, elapsed_dgbtrf, nbpoints, elapsed_dgbtrs, nbpoints,
+	       elapsed_dgbsv, nbpoints, elapsed_dgbtrftridiag, nbpoints,
+	       forward_error);
+	free(RHS);
+	free(EX_SOL);
+	free(X);
+	free(AB);
+	//    printf("\n\n--------- End -----------\n");
 }
---
+++ src/tp_poisson1D_iter.c
@@ -0,0 +1,127 @@
+/******************************************/
+/* tp2_poisson1D_iter.c                 */
+/* This file contains the main function   */
+/* to solve the Poisson 1D problem        */
+/******************************************/
+#include "lib_poisson1D.h"
+#include "atlas_headers.h"
+
+int main(int argc, char *argv[])
+/* ** argc: Number of arguments */
+/* ** argv: Values of arguments */
+{
+	int ierr;
+	int jj;
+	int nbpoints, la;
+	int ku, kl, lab, kv;
+	int *ipiv;
+	int info;
+	int NRHS;
+	double T0, T1;
+	double *RHS, *SOL, *EX_SOL, *X;
+	double *AB;
+	double *MB;
+	f64 *tmp_vec;
+	double temp, relres;
+
+	double opt_alpha, forward_error;
+
+	/* Size of the problem */
+	NRHS = 1;
+	nbpoints = 12;
+	la = nbpoints - 2;
+
+	/* Dirichlet Boundary conditions */
+	T0 = 5.0;
+	T1 = 20.0;
+
+	printf("--------- Poisson 1D ---------\n\n");
+	RHS = (double *)malloc(sizeof(double) * la);
+	SOL = (double *)calloc(la, sizeof(double));
+	EX_SOL = (double *)malloc(sizeof(double) * la);
+	X = (double *)malloc(sizeof(double) * la);
+	tmp_vec = malloc(sizeof(double) * la);
+	/* Setup the Poisson 1D problem */
+	/* General Band Storage */
+	set_grid_points_1D(X, &la);
+	set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
+	set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
+
+	write_vec(RHS, &la, "RHS.dat");
+	write_vec(EX_SOL, &la, "EX_SOL.dat");
+	write_vec(X, &la, "X_grid.dat");
+
+	kv = 0;
+	ku = 1;
+	kl = 1;
+	lab = kv + kl + ku + 1;
+
+	AB = (double *)malloc(sizeof(double) * lab * la);
+	set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
+
+	/* uncomment the following to check matrix A */
+	// write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
+
+	/********************************************/
+	/* Solution (Richardson with optimal alpha) */
+
+	/* Computation of optimum alpha */
+	opt_alpha = richardson_alpha_opt(&la);
+	printf("Optimal alpha for simple Richardson iteration is : %lf",
+	       opt_alpha);
+
+	/* Solve */
+	double tol = 1e-3;
+	int maxit = 1000;
+	double *resvec;
+	int nbite = 0;
+
+	resvec = (double *)calloc(maxit, sizeof(double));
+
+	/* Solve with Richardson alpha */
+	// richardson_alpha(AB, RHS, SOL, &opt_alpha, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+
+	/* Richardson General Tridiag */
+
+	/* get MB (:=M, D for Jacobi, (D-E) for Gauss-seidel) */
+	kv = 1;
+	ku = 1;
+	kl = 1;
+	MB = (double *)malloc(sizeof(double) * (lab)*la);
+	// extract_MB_jacobi_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+	// write_GB_operator_colMajor_poisson1D(MB, &lab, &la, "JACOBI.dat");
+
+	// extract_MB_gauss_seidel_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+	// write_GB_operator_colMajor_poisson1D(MB, &lab, &la, "GAUSS-SEIDEL.dat");
+
+	/* Solve with General Richardson */
+	// richardson_MB(AB, RHS, SOL, MB, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+
+	/* Forward error :    ||x-xø|| / ||x|| */
+
+	memcpy(tmp_vec, RHS, la * sizeof(f64));
+
+	cblas_daxpy(la, -1, EX_SOL, 1, tmp_vec, 1);
+
+	f64 norm_x_xø = cblas_dnrm2(la, tmp_vec, 1);
+
+	f64 norm_x = cblas_dnrm2(la, EX_SOL, 1);
+
+	forward_error = norm_x_xø / norm_x;
+
+	printf("\n%lf\n", forward_error);
+	/* Write solution */
+	write_vec(SOL, &la, "SOL.dat");
+
+	/* Write convergence history */
+	write_vec(resvec, &nbite, "RESVEC.dat");
+
+	free(resvec);
+	free(RHS);
+	free(SOL);
+	free(EX_SOL);
+	free(X);
+	free(AB);
+	free(MB);
+	printf("\n\n--------- End -----------\n");
+}
